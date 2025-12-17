b6a23c24539aa2e1595696c4b8d92b3bd6a04e68
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-17 16:21:26 +0530

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+module.exports = {
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        region: process.env.AWS_REGION,
+        params: {
+          Bucket: process.env.AWS_S3_BUCKET,
+        },
+      },
+    },
+  },
+};
