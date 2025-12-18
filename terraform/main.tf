807a2124de5bd860fac0dac484cb99249b6aeadf
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-18 11:32:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,8 +132,16 @@ resource "aws_ecs_service" "strapi" {
   launch_type     	= "FARGATE"
 
   network_configuration {
-    subnets                  	     = data.aws_subnets.default.ids
+    subnets                  = data.aws_subnets.default.ids
     security_groups          = [aws_security_group.strapi_sg.id]
     assign_public_ip          = true
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.strapi.arn
+    container_name   = "strapi"
+    container_port   = 1337
+  }
+
+  depends_on = [aws_lb_listener.http]
 }
