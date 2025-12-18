ca7aa584de04f01b85993964130031321546df4b
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-18 11:33:29 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,1 +1,79 @@
+# Get default VPC
+data "aws_vpcs" "default" {
+  filter {
+    name   = "isDefault"
+    values = ["true"]
+  }
+}
 
+locals {
+  default_vpc_id = data.aws_vpcs.default.ids[0]
+}
+
+# Get default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [local.default_vpc_id]
+  }
+}
+
+# security group for alb
+resource "aws_security_group" "alb" {
+  name        = "docker-strapi-alb-sg"
+  description = "Allow HTTP inbound"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+3 target group for application load balancer
+resource "aws_lb_target_group" "strapi" {
+  name        = "docker-strapi-tg2"
+  port        = 1337
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+}
+
+
+resource "aws_lb" "strapi" {
+  name               = "docker-strapi-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+# load balancer listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.strapi.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.strapi.arn
+  }
+}
